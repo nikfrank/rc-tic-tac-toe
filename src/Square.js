af39aba9c7dccf6b0fe467f84c4cc1316acbf023
nik frank <nikbfrank@gmail.com>
2021-03-11 13:21:28 +0200

    course notes from class

--- src/Square.js
+++ src/Square.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
 export const Square = ({
-  tick = '',
-  onClick = ()=>0,
-})=> (
-  <svg viewBox='0 0 10 10' onClick={tick ? undefined : onClick}>
-    {tick === 'X' ? (
-      <path d='M 0 1 L 1 0 L 5 4 L 9 0 L 10 1 L 6 5 L 10 9 L 9 10 L 5 6 L 1 10 L 0 9 L 4 5 Z' />
-    ) : tick === 'O' ? (
-      <circle cx={5} cy={5} r={4}/>
-    ) : null}
-  </svg>
-);
+  value,
+})=>{
+  return (
+    <svg viewBox='0 0 10 10'>
+      {
+        value === 'O' ? (
+          <circle cx={5} cy={5} r={3} />
+        ) : value === 'X' ? (
+          <path d='M 1 0 L 5 4 L 9 0 L 10 1 L 6 5 L 10 9 L 9 10 L 5 6 L 1 10 L 0 9 L 4 5 L 0 1 Z' />
+        ) : null
+      }
+    </svg>
+  );
+};
